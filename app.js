a3e058fb278f19238b7efcef343ced337eac495c
mihawon <kmghope7@gmail.com>
2024-09-09 20:14:42 +0900

    all new

---
+++ app.js
@@ -0,0 +1,19 @@
+// let promise = fetch(url,{method: "Delete"})
+// let promise = fetch(url,{method: "Post"})
+// let promise = fetch(url,{method: "Get"})
+// let promise = fetch(url,{method: "Put"})
+function login() { 
+	alert("로그인 합니다!");
+}
+
+function register() { 
+	alert("계정이 제작되었습니다!");
+}
+
+function chara_make() {
+	alert("캐릭터가 생성되었습니다!");
+}
+
+function item_make() {
+	alert("아이템이 생성되었습니다!");
+}
